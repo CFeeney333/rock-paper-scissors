63fee81edd4b4ee022324682c961f2f0323edad6
CFeeney333 <c.feeney333@gmail.com>
2022-05-11 13:43:46 +0100

    Implement playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,6 +4,25 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     // compare selections and return string declaring winner
+
+    // We will work with lower case, and we want to accept mixed case from the user
+    playerSelection = playerSelection.toLowerCase();
+
+    // flag to determine message to return
+    let playerWins = false;
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerWins = true;
+    } else {
+        playerWins = false;
+    }
+
+    if (playerWins) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`; 
+    }
 }
 
 function game() {
