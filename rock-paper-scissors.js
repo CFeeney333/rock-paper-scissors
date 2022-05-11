901609a598e9b628fedc0f61f5ff5d83f2f8940e
CFeeney333 <c.feeney333@gmail.com>
2022-05-11 13:31:33 +0100

    Create function skeletons with basic logging to start

---
+++ rock-paper-scissors.js
@@ -0,0 +1,17 @@
+function computerPlay() {
+    // return randomly rock, paper or scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+    // compare selections and return string declaring winner
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
