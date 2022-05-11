49b23406ad72b35e5f6e07de5718519ecbd65146
CFeeney333 <c.feeney333@gmail.com>
2022-05-11 16:05:07 +0100

    Implement game loop function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,15 @@
 function computerPlay() {
     // return randomly rock, paper or scissors
-    return 'rock';
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        default:
+            return 'scissors';
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -28,10 +37,6 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     for (let i = 0; i < 5; i++) {
-
+        console.log(playRound(prompt("Rock, paper, or scissors:"), computerPlay()));  
     }
 }
-
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
